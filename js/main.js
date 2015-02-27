c0d45aa04cf5a119a8bf56b1106f4050a480fcfe
Cathy Chen <cathychenn@gmail.com>
2015-02-27 11:58:26 -0500

    a lot of shit

--- js/main.js
+++ js/main.js
@@ -3,22 +3,27 @@ var state = 0;
 
 var inputData = [];
 
-var fips1 = [incoming1, outgoing1];
-var fips2 = [incoming2, outgoing2];
-
-var incoming1 = [county_count2_in, county_count3_in];
-var incoming2 = [county_count1_in, county_count3_in];
-
-var outgoing1 = [county_count2_out, county_count3_out];
-var outgoing2 = [county_count1_out, county_count3_out];
-
 var county_count1_in = ["c06071", 100];
 var county_count2_in = ["c48043", 50];
 var county_count3_in = ["c48479", 80];
+var county_count4_in = ["c51630", 10];
 
 var county_count1_out = ["c06071", 2];
 var county_count2_out = ["c48043", 1];
 var county_count3_out = ["c48479", 8];
+var county_count4_out = ["c51630", 7];
+
+
+var incoming1 = [county_count2_in, county_count3_in, county_count4_in];
+var incoming2 = [county_count1_in, county_count3_in, county_count4_in];
+
+var outgoing1 = [county_count2_out, county_count3_out, county_count4_out];
+var outgoing2 = [county_count1_out, county_count3_out, county_count4_out];
+
+var fips1 = [incoming1, outgoing1];
+var fips2 = [incoming2, outgoing2];
+
+
 
 inputData["c06071"] = fips1;
 inputData["c48043"] = fips2;
@@ -34,6 +39,8 @@ $(document).ready(function() {
 	$('.results').hide();
 
 	console.log(inputData);
+
+	setup();
 
 	// TO DO
 	// color counties with Hertz yellow to start
@@ -100,7 +107,7 @@ $(document).ready(function() {
 			state = 0;
 		}
 
-    });
+	});
 });
 
 // svg set up - color counties with Hertz yellow
@@ -138,12 +145,18 @@ function leaving(county) {
 	$('#leaving').empty();
 
 	var county_name = county.attr("id");
-	console.log(county_name);
+	var fips = county.attr("class");
 
-	
-	// var top1 = ;
-	// var top2 =;
-	// var top3 =;
+	console.log(fips);
+	if (typeof inputData[fips] != 'undefined') {
+		console.log("HERHERE");
+		console.log("******" + inputData[fips] + "*****")
+		var top1 = inputData[fips][1][0];
+		var top2 = inputData[fips][1][1];
+		var top3 = inputData[fips][1][2];
+	}
+
+	console.log(top1);
 
 	$('#leaving')
 	.append(
@@ -152,13 +165,13 @@ function leaving(county) {
 			$('<div>').attr("class", "hd").html("Outgoing")
 			)
 		.append(
-			$('<div>').attr("class", "data").html(county_name)
+			$('<div>').attr("class", "data").html(top1[0] + " : " + top1[1])
 			)
 		.append(
-			$('<div>').attr("class", "data").html(county_name)
+			$('<div>').attr("class", "data").html(top2[0] + " : " + top2[1])
 			)
 		.append(
-			$('<div>').attr("class", "data").html(county_name)
+			$('<div>').attr("class", "data").html(top3[0] + " : " + top3[1])
 			)
 		);
 }
@@ -167,11 +180,15 @@ function arriving(county) {
 	$('#arriving').empty();
 
 	var county_name = county.attr("id");
-	console.log(county_name);
+	var fips = county.attr("class");
 
-	// var top1 =;
-	// var top2 =;
-	// var top3 =;
+	if (typeof inputData[fips] != 'undefined') {
+		var top1 = inputData[fips][0][0];
+		var top2 = inputData[fips][0][1];
+		var top3 = inputData[fips][0][2];
+	}
+
+	console.log(top1);
 
 	$('#arriving')
 	.append(
@@ -180,15 +197,20 @@ function arriving(county) {
 			$('<div>').attr("class", "hd").html("Incoming")
 			)
 		.append(
-			$('<div>').attr("class", "data").html(county_name)
+			$('<div>').attr("class", "data").html(top1[0] + " : " + top1[1])
 			)
 		.append(
-			$('<div>').attr("class", "data").html(county_name)
+			$('<div>').attr("class", "data").html(top2[0] + " : " + top2[1])
 			)
 		.append(
-			$('<div>').attr("class", "data").html(county_name)
+			$('<div>').attr("class", "data").html(top3[0] + " : " + top3[1])
 			)
 		);
+}
+
+function setup() {
+
+	
 }
 
 
